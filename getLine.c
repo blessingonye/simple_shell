c13d6a5f73d6eff0f50bdd17e21ac472b085adaa
Chrisnellincoln <nelsonchristian992@gmail.com>
2023-05-23 13:37:17 -0700

    getLine.c

--- getLine.c
+++ getLine.c
@@ -11,7 +11,7 @@
 ssize_t input_buf(info_t *info, char **buf, size_t *len)
 {
 	ssize_t r = 0;
-	size_t len_p = 0;
+	size_t input_buf = 0;
 
 	if (!*len) /* if nothing left in the buffer, fill it */
 	{
